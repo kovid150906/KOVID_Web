fc524a52c846708e133e3f4f3ffe8390d3fb7c49
Kovid <kovid611@gmail.com>
2025-07-17 17:08:04 +0530

    scroll to top on changing pages and ek scroll to top ka arrow add kra hai(added scroll to top feature and arrow)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Team from './pages/Team'
-import Tasks from './pages/Tasks'
-import Sidebar from './components/Sidebar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Team from './pages/Team';
+import Tasks from './pages/Tasks';
+import Sidebar from './components/Sidebar';
+import ScrollToTop from './components/ScrollToTop';
+import ScrollToTopButton from './components/ScrollToTopButton';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#0a0a0a] overflow-x-hidden">
+    <div className="min-h-screen bg-[#0a0a0a] overflow-x-hidden relative">
       <Router>
+        <ScrollToTop />
         <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/team" element={<Team />} />
           <Route path="/tasks" element={<Tasks />} />
         </Routes>
+        <ScrollToTopButton />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 
